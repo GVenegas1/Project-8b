faafe90eef2cc3c1c498cdc0412e832aaeee220e
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-11-19 21:36:04 -0800

    Reverse_list

--- reverse_list.py
+++ reverse_list.py
@@ -6,12 +6,20 @@
 #Date: 11/19/2025
 
 def reverse_list(list):
+    #Two pointers: one at the start and one at the end
     start = 0
+    #last index in the list
     end = len(list) - 1
 
+#Keep swapping elements until the two pointers meet in the middle
     while start < end:
+        #Swap the element at the start and end
         list[start], list[end] = list[end], list[start]
+
+        #Move the pointers towards the middle
+        #pointer moves forward to the next element
         start += 1
+        #pointer moves backwards to the previous element
         end -= 1
 
 #example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
