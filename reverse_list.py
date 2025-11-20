c142ade5821eed7f21eba5d7cc2ec1ceda5d4fbe
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-11-19 23:00:10 -0800

    Reverse_list

--- reverse_list.py
+++ reverse_list.py
@@ -4,21 +4,27 @@
 #Gabriel Venegas
 #GitHub username: GVenegas1
 #Date: 11/19/2025
+"""This function changes the order of the list in reverse.
+It mutates the list and doesn't return anything """
 
 def reverse_list(list):
+
     #Two pointers: one at the start and one at the end
     start = 0
+
     #last index in the list
     end = len(list) - 1
 
 #Keep swapping elements until the two pointers meet in the middle
     while start < end:
+
         #Swap the element at the start and end
         list[start], list[end] = list[end], list[start]
 
         #Move the pointers towards the middle
         #pointer moves forward to the next element
         start += 1
+
         #pointer moves backwards to the previous element
         end -= 1
 
